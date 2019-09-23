a6c1498af283ff3c715e9432001df8f91985a29c
ziyanjupan <1258811819@qq.com>
2019-09-23 16:21:16 +0800

    上传

--- base/src/main/java/com/fenix/base/controllers/DownloadController.java
+++ base/src/main/java/com/fenix/base/controllers/DownloadController.java
@@ -1,13 +1,13 @@
 package com.fenix.base.controllers;
 
+import com.fenix.base.DTO.UploadDTO;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,7 +16,18 @@ import java.io.*;
 @RestController
 public class DownloadController {
 
-    @RequestMapping(value = "/downloadFile",method = RequestMethod.GET)
+    @RequestMapping(value = "/downloadFile", method = RequestMethod.POST)
+    @ResponseBody
+    public void download(@RequestParam(name = "file", required = false) MultipartFile file, UploadDTO uploadDTO) {
+        if (file.isEmpty()) {
+
+        }
+        if (uploadDTO == null) {
+
+        }
+    }
+
+    @RequestMapping(value = "/downloadFile1", method = RequestMethod.GET)
     private String downloadFile(HttpServletResponse response) {
 //        String downloadFilePath = "/root/fileSavePath/";//被下载的文件在服务器中的路径,
 //        String fileName = "demo.xml";//被下载文件的名称
@@ -24,39 +35,39 @@ public class DownloadController {
 
 //        File file = new File(downloadFilePath);
 //        if (file.exists()) {
-            response.setContentType("application/force-download");// 设置强制下载不打开            
-            response.addHeader("Content-Disposition", "attachment;fileName=abc.jpg");
-            byte[] buffer = new byte[1024];
-            FileInputStream fis = null;
-            BufferedInputStream bis = null;
-            try {
-                fis = new FileInputStream(path);
-                bis = new BufferedInputStream(fis);
-                OutputStream outputStream = response.getOutputStream();
-                int i = bis.read(buffer);
-                while (i != -1) {
-                    outputStream.write(buffer, 0, i);
-                    i = bis.read(buffer);
-                }
-                return "下载成功";
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                if (bis != null) {
-                    try {
-                        bis.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                if (fis != null) {
-                    try {
-                        fis.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+        response.setContentType("application/force-download");// 设置强制下载不打开            
+        response.addHeader("Content-Disposition", "attachment;fileName=abc.jpg");
+        byte[] buffer = new byte[1024];
+        FileInputStream fis = null;
+        BufferedInputStream bis = null;
+        try {
+            fis = new FileInputStream(path);
+            bis = new BufferedInputStream(fis);
+            OutputStream outputStream = response.getOutputStream();
+            int i = bis.read(buffer);
+            while (i != -1) {
+                outputStream.write(buffer, 0, i);
+                i = bis.read(buffer);
+            }
+            return "下载成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (bis != null) {
+                try {
+                    bis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
             }
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
 //        }
         return "下载失败";
     }
